e9557a688a32d5a575430bc620a1fa7321492441
Aaron <aaron@1coin.io>
2023-05-01 20:39:30 +0000

    fix

--- src/constants/chainInfo.ts
+++ src/constants/chainInfo.ts
@@ -253,7 +253,7 @@ export function getChainInfo(chainId: any): any {
   return undefined
 }
 
-const MAINNET_INFO = CHAIN_INFO[SupportedChainId.MAINNET]
+const MAINNET_INFO = CHAIN_INFO[SupportedChainId.BOMB]
 export function getChainInfoOrDefault(chainId: number | undefined) {
   return getChainInfo(chainId) ?? MAINNET_INFO
 }
